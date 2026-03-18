85663c98257067524281412e930b12430e9cf56e
Vianney7000 <vianneyobodozie8@gmail.com>
2026-03-18 02:21:43 -0700

    Fix CORS issue

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import  dotenv  from 'dotenv'
 import express from 'express'
+import cors from "cors"
 import connectDB from "./config/db.js"
 import userRoute from './routes/user_route.js'
 
@@ -7,6 +8,8 @@ import userRoute from './routes/user_route.js'
 dotenv.config()
 
 const app = express()
+app.use(cors());
+
 app.use(express.json())
 
 
