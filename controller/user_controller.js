376c3ea1cbf0b09bff954dd9db965e7008631d66
Vianney7000 <vianneyobodozie8@gmail.com>
2026-04-02 00:48:28 -0700

    fixed role

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -6,14 +6,18 @@ import jwt from 'jsonwebtoken';
 // REGISTER
 export const register = async (req, res) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: 'Please fill in all fields' });
+    }
 
   const existingUser = await User.findOne({ email });
    if (existingUser) return res.status(400).json({ message: 'User already exists' });
 
 const hashedPassword = await bcrypt.hash(password, 10);
 
-const newUser = new User({ name, email, password: hashedPassword, role});
+const newUser = new User({ name, email, password: hashedPassword });
 await newUser.save();
 
     res.status(201).json({ message: 'User registered successfully' });
