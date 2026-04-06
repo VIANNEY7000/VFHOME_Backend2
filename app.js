08094d5e792db811f2da247cf48e84960478c4fe
Vianney7000 <vianneyobodozie8@gmail.com>
2026-04-06 07:32:38 -0700

    added order logic

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ dotenv.config()
 const app = express()
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // ROUTE
 app.use('/api/auth', userRoute)
