87de86dd0288441f0337cefc87f204e5056cd0cd
Vianney7000 <vianneyobodozie8@gmail.com>
2026-04-11 02:57:41 -0700

    ser id integrayions

--- controller/order_controller.js
+++ controller/order_controller.js
@@ -1,5 +1,50 @@
 import { Order } from "../models/order_model.js";
 
+
+// CREATEB ORDER
+export const createOrder = async (req, res) => {
+  try {
+    const {
+      fullName,
+      email,
+      phone,
+      address,
+      city,
+      state,
+      country,
+      items,
+      totalPrice
+    } = req.body;
+
+    if (!items || items.length === 0) {
+      return res.status(400).json({ message: "No items in order" });
+    }
+
+    const order = await Order.create({
+      fullName,
+      email,
+      phone,
+      address,
+      city,
+      state,
+      country,
+      items,
+      totalPrice,
+      paymentStatus: "Pending",
+      orderStatus: "Pending"
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Order created successfully",
+      order
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 // Get All Orders (Admin)
 export const getAllOrders = async (req, res) => {
   try {
@@ -9,6 +54,23 @@ export const getAllOrders = async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error", error: error.message });
   }
 };
+
+// GET USER ORDER
+export const getUserOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ email: req.user.email })
+      .sort({ createdAt: -1 });
+
+    res.json({
+      success: true,
+      orders
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 
 // Update Order Status
 export const updateOrderStatus = async (req, res) => {
--- routes/order_route.js
+++ routes/order_route.js
@@ -1,12 +1,18 @@
 import express from "express";
 import {
+  createOrder,
   getAllOrders,
+  getUserOrders,
   updateOrderStatus
 } from "../controller/order_controller.js";
+import { verifyToken, isAdmin } from '../middleware/auth_middleware.js';
+
 
 const router = express.Router();
 
-router.get("/", getAllOrders);
-router.put("/update/:id", updateOrderStatus);
+router.post("/create", verifyToken, createOrder);
+router.get("/my-orders", verifyToken, getUserOrders);
+router.get("/", verifyToken, isAdmin, getAllOrders);
+router.put("/update/:id", verifyToken, isAdmin, updateOrderStatus);
 
 export default router;
